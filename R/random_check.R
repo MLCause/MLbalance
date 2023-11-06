137188727a9b7915b21393a2213dd5890f92dffc
Jack T. Rametta <jtrametta@ucdavis.edu>
2023-11-06 09:26:38 -0800

    fix if/any issue in main function

--- R/random_check.R
+++ R/random_check.R
@@ -148,7 +148,7 @@ random_check <- function(W_real, W_sim = NULL, X,R.seed = 1995, grf.seed = 1995,
     "plot" = g)
 
   # Add marker for detection of extreme treatment propensities
-  results$extreme.props <- if(results$treat.props > .9 | results$treat.props < .1){
+  results$extreme.props <- if(any(results$treat.props > .9 | results$treat.props < .1)){
                                   cat("\n\nWarning: Extreme Propensity scores detected (greater than .9 or less than .1).
                                       Examine $treat.props for more detail. \n\n")} else {
                                   cat("\n\nNo Extreme Propensity scores detected (greater than .9 or less than .1). \n\n")
